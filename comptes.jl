33e6ffdca38c9edee7d52609fc96d53ccb334442
Randy NKAMWA <118863581+randynk@users.noreply.github.com>
2023-03-06 17:14:45 +0100

    Update comptes.jl
    A chaque transaction, l'algorithme renvoie les statistiques des contributions

--- comptes.jl
+++ comptes.jl
@@ -1,6 +1,7 @@
 function first()
-    touch("bilan_montants.txt")
-    touch("bilan_noms.txt")
+    touch("ofoe4534opzaeu12/mlbshye")
+    touch("ofoe4534opzaeu12/asxfthnjil")
+    touch("ofoe4534opzaeu12/plknbgresw")
 end
 
 function asking()
@@ -40,23 +41,23 @@ function search(fichier::String)
 end
 
 function update(index)
-    names = open("bilan_noms.txt","r")
-    amounts = open("bilan_montants.txt","r")
+    names = open("ofoe4534opzaeu12/plknbgresw","r")
+    amounts = open("ofoe4534opzaeu12/asxfthnjil","r")
     name = readlines(names)
     amount = readlines(amounts)
     close(names)
     close(amounts)
 
     if typeof(index) == Nothing
-        noms = open("bilan_noms.txt","a")
-        montants = open("bilan_montants.txt","a")
+        noms = open("ofoe4534opzaeu12/plknbgresw","a")
+        montants = open("ofoe4534opzaeu12/asxfthnjil","a")
         write(noms, "$(data["nom"])\n")
         write(montants, "$(data["montant"])\n")
         close(noms)
         close(montants)
         sleep(1)
     else
-        updated = parse(Int64, amount[index]) + parse(Int64, data["montant"])
+        updated = parse(Float64, amount[index]) + parse(Float64, data["montant"])
         updated = string(updated, "")
         amount = setindex!(amount, updated, index)
         sleep(1)
@@ -65,15 +66,76 @@ function update(index)
             write(news, "$j\n")
         end
         close(news)
-        rm("bilan_montants.txt")
-        cp("montants.txt","bilan_montants.txt")
+        rm("ofoe4534opzaeu12/asxfthnjil")
+        cp("montants.txt","ofoe4534opzaeu12/asxfthnjil")
         rm("montants.txt")
     end
     rm("noms_temporaires.txt")
     rm("montants_temporaires.txt")
 end
 
+function solde()
+    touch("ofoe4534opzaeu12/mlbshye")
+    try
+        lireLeSolde = open("ofoe4534opzaeu12/mlbshye","r")
+        begin
+            global ancienSolde = parse(Float64, (readline(lireLeSolde)))
+        end
+        close(lireLeSolde)
+    catch
+        begin
+            global ancienSolde = parse(Float64, "0")
+        end
+    end
+    nouveauSolde = ancienSolde + parse(Float64, data["montant"])
+    ecrireLeSolde = open("ofoe4534opzaeu12/mlbshye","w")
+    write(ecrireLeSolde, string(nouveauSolde))
+    close(ecrireLeSolde)
+end
+try
+    rm("ofoe4534opzaeu12/ygvdzqwxd")
+    rm("ofoe4534opzaeu12/tdwqazedc") 
+catch
+end
+
+function classify()
+    nouveau_Solde = open("ofoe4534opzaeu12/mlbshye","r")
+    nouveauSolde = readline(nouveau_Solde)
+    close(nouveau_Solde)
+
+    montant = Float64[]
+    nom = []
+    
+    montants = open("ofoe4534opzaeu12/ygvdzqwxd","r")
+    for i in eachline(montants)
+        push!(montant, parse(Float64, i))
+    end
+    noms = open("ofoe4534opzaeu12/tdwqazedc","r")
+    for i in eachline(noms)
+        push!(nom, i)
+    end 
+println(montant)
+    montant = map(x -> x * 100 / (parse(Float64, nouveauSolde)), montant)
+println(montant)
+    percents = open("soldes.txt","w")
+    begin
+        percent = ""
+    end
+    for i in eachindex(montant)
+        begin
+            percent = string(percent, nom[i], "        |        ", montant[i], " %\n")    
+        end
+    end
+    write(percents, percent)
+    write(percents, "\n::\n::\n\n Solde : $(BigFloat(nouveauSolde))")
+    close(percents)
+end
+
 first()
 asking()
 datas()
-update(search("bilan_noms.txt"))
+update(search("ofoe4534opzaeu12/plknbgresw"))
+cp("ofoe4534opzaeu12/asxfthnjil","ofoe4534opzaeu12/ygvdzqwxd")
+cp("ofoe4534opzaeu12/plknbgresw","ofoe4534opzaeu12/tdwqazedc")
+solde()
+classify()
